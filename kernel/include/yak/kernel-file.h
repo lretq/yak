e6c99637a0cc7145d18d4fbebcf444b7ac47f8e7
Vincent Schweiger <vincent.schweiger@xolley.de>
2025-05-31 21:37:12 +0200

    add symbol definitions

---
+++ kernel/include/yak/kernel-file.h
@@ -0,0 +1,16 @@
+#pragma once
+
+extern char __kernel_limine_start[];
+extern char __kernel_limine_end[];
+
+extern char __kernel_text_start[];
+extern char __kernel_text_end[];
+
+extern char __kernel_rodata_start[];
+extern char __kernel_rodata_end[];
+
+extern char __kernel_data_start[];
+extern char __kernel_data_end[];
+
+extern char __kernel_percpu_start[];
+extern char __kernel_percpu_end[];
