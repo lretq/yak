d6d6ba97dd480cfa74275a0e17abec4a3d17dd10
Vincent Schweiger <vincent.schweiger@xolley.de>
2025-06-05 20:35:20 +0200

    preliminary bump based vm_map_alloc

--- kernel/include/yak/vm/map.h
+++ kernel/include/yak/vm/map.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <yak/vm/pmap.h>
+#include <yak/status.h>
 
 struct vm_map {
 	struct pmap pmap;
 };
 
-extern struct vm_map kernel_map;
+struct vm_map *kmap();
+
+status_t vm_map_alloc(struct vm_map *map, size_t length, uintptr_t *out);
--- kernel/src/vm/map.c
+++ kernel/src/vm/map.c
@@ -1,3 +1,26 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <assert.h>
 #include <yak/vm/map.h>
+#include <yak/macro.h>
+#include <yak/arch-mm.h>
+#include <yak/status.h>
 
-struct vm_map kernel_map;
+static struct vm_map kernel_map;
+
+struct vm_map *kmap()
+{
+	return &kernel_map;
+}
+
+// TODO: proper virtual memory allocator
+// -> VMEM etc...
+static uintptr_t kernel_arena_base = KMAP_ARENA_BASE;
+
+status_t vm_map_alloc(struct vm_map *map, size_t length, uintptr_t *out)
+{
+	assert(IS_ALIGNED_POW2(length, PAGE_SIZE));
+	(void)map;
+	*out = __atomic_fetch_add(&kernel_arena_base, length, __ATOMIC_RELAXED);
+	return YAK_SUCCESS;
+}
