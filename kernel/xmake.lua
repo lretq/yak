3d9de2c1eea6fb4f8b72bd124888708948dd5699
Vincent Schweiger <vincent.schweiger@xolley.de>
2025-06-07 12:57:08 +0200

    setup uacpi dependency

--- kernel/xmake.lua
+++ kernel/xmake.lua
@@ -20,7 +20,7 @@ target("yak.headers")
 target("yak.deps")
 	set_default(false)
 	set_kind("phony")
-	add_deps("yak.headers", "nanoprintf", "cc-runtime")
+	add_deps("yak.headers", "nanoprintf", "cc-runtime", "uacpi")
 
 rule("kernel")
 	on_load(function(target)
