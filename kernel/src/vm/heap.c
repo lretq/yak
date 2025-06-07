9e762c7a42085d0d78b0ae780e3b9f266faaabfc
Vincent Schweiger <vincent.schweiger@xolley.de>
2025-06-07 18:12:55 +0200

    stupidly simple BUMP heap

---
+++ kernel/src/vm/heap.c
@@ -0,0 +1,37 @@
+#include <stdint.h>
+#include <yak/heap.h>
+#include <yak/macro.h>
+#include <yak/vm/map.h>
+#include <yak/arch-mm.h>
+#include <yak/vm/pmap.h>
+#include <yak/vm/pmm.h>
+#include <yak/panic.h>
+
+static uintptr_t heap_base;
+static uintptr_t heap_end;
+
+void heap_init()
+{
+	EXPECT(vm_map_alloc(kmap(), PAGE_SIZE * 2048, &heap_base));
+	heap_end = heap_base + PAGE_SIZE * 2048;
+	for (uintptr_t i = heap_base; i < heap_end; i += PAGE_SIZE) {
+		pmap_map(&kmap()->pmap, i, pmm_alloc(), 0, VM_RW,
+			 VM_CACHE_DEFAULT);
+	}
+}
+
+void *kmalloc(size_t size)
+{
+	size = ALIGN_UP(size, 16);
+	uintptr_t addr = __atomic_fetch_add(&heap_base, size, __ATOMIC_RELAXED);
+	if (addr >= heap_end) {
+		panic("Heap OOM");
+	}
+	return (void *)addr;
+}
+
+void kfree(void *ptr, size_t size)
+{
+	(void)ptr;
+	(void)size;
+}
