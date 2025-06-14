ba325dbcebc63663ba07c2480ea4a0f4fc8c55c1
Vincent Schweiger <vincent.schweiger@xolley.de>
2025-06-14 14:50:56 +0200

    conditionally compile asserts

--- kernel/include/assert.h
+++ kernel/include/assert.h
@@ -7,6 +7,11 @@ void __assert_fail(const char *assertion, const char *file, unsigned int line,
 		   const char *function);
 
 #undef assert
+
+#ifdef CONFIG_DEBUG
 #define assert(expr)              \
 	((void)(likely((expr)) || \
 		(__assert_fail(#expr, __FILE__, __LINE__, __func__), 0)))
+#else
+#define assert(expr)
+#endif
