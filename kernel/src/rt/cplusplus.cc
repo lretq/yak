c4213956c458401b7fb371aec9b818bc6ad88770
Vincent Schweiger <vincent.schweiger@xolley.de>
2025-06-21 02:04:32 +0200

    c++ operators/compiler stuff

--- kernel/src/rt/cplusplus.cc
+++ kernel/src/rt/cplusplus.cc
@@ -7,6 +7,11 @@ void __cxa_pure_virtual()
 {
 	panic("__cxa_pure_virtual");
 }
+
+void __cxa_deleted_virtual()
+{
+	panic("__cxa_deleted_virtual");
+}
 }
 
 void *operator new(std::size_t size)
@@ -17,4 +22,21 @@ void *operator new(std::size_t size)
 void operator delete(void *ptr, size_t size) noexcept
 {
 	kfree(ptr, size);
+}
+
+void *operator new[](size_t size)
+{
+	size = sizeof(size_t) + size;
+	size_t *ptr = (size_t *)kmalloc(size);
+	*ptr = size;
+	return ++ptr;
+}
+
+void operator delete[](void *ptr)
+{
+	if (!ptr)
+		return;
+	size_t *szptr = (size_t *)ptr;
+	--szptr;
+	kfree(szptr, *szptr);
 }
