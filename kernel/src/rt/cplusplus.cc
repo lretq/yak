4c0cba18595b827db0b2cf335326b8992bc6a6b6
Vincent Schweiger <vincent.schweiger@xolley.de>
2025-06-29 13:02:47 +0200

    implement (hopefully) correct new[]

--- kernel/src/rt/cplusplus.cc
+++ kernel/src/rt/cplusplus.cc
@@ -1,6 +1,7 @@
 #include <cstddef>
 #include <yak/heap.h>
 #include <yak/panic.h>
+#include <yak/macro.h>
 
 extern "C" {
 void __cxa_pure_virtual()
@@ -11,6 +12,10 @@ void __cxa_pure_virtual()
 void __cxa_deleted_virtual()
 {
 	panic("__cxa_deleted_virtual");
+}
+
+void __cxa_atexit()
+{
 }
 }
 
@@ -24,19 +29,24 @@ void operator delete(void *ptr, size_t size) noexcept
 	kfree(ptr, size);
 }
 
+constexpr size_t HEADER_SIZE =
+	ALIGN_UP(sizeof(size_t), alignof(std::max_align_t));
+
 void *operator new[](size_t size)
 {
-	size = sizeof(size_t) + size;
-	size_t *ptr = (size_t *)kmalloc(size);
+	size_t real_size = HEADER_SIZE + size;
+	size_t *ptr = (size_t *)kmalloc(real_size);
+	if (ptr == nullptr)
+		panic("new[] oom");
 	*ptr = size;
-	return ++ptr;
+	return (void *)((uintptr_t)ptr + HEADER_SIZE);
 }
 
 void operator delete[](void *ptr)
 {
 	if (!ptr)
 		return;
-	size_t *szptr = (size_t *)ptr;
-	--szptr;
+
+	size_t *szptr = (size_t *)((uintptr_t)ptr - HEADER_SIZE);
 	kfree(szptr, *szptr);
 }
