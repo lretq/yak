8205c8b4f4c143937896e940f2d4d3de94e532aa
Vincent Schweiger <vincent.schweiger@xolley.de>
2025-08-17 17:22:36 +0200

    util: create byte size macros

---
+++ kernel/include/yak/byte-macros.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define KiB(o) ((size_t)(1L << 10) * (o))
+#define MiB(o) ((size_t)(1L << 20) * (o))
+#define GiB(o) ((size_t)(1L << 30) * (o))
+#define TiB(o) ((size_t)(1L << 40) * (o))
