f981e2f6c8a3c6783e15506365707220f61c4947
Vincent Schweiger <vincent.schweiger@xolley.de>
2025-08-18 14:36:33 +0200

    generalize cleanup.h

--- kernel/include/yak/cleanup.h
+++ kernel/include/yak/cleanup.h
@@ -9,33 +9,36 @@
 #endif
 
 #include <yak/mutex.h>
+#include <yak/sched.h>
 #include <yak/log.h>
 
-#define cleanup_class(name, members, fn_body)   \
-	typedef struct cleanup_class_name {     \
-	} cleanup_class_##name##_t;             \
-	struct cleanup_##name members;          \
-	static inline void cleanup_##name##_fn( \
-		[[maybe_unused]] struct cleanup_##name *ctx) fn_body
+#define DEFINE_CLEANUP_CLASS(name, members, fn_body, init_body, init_args...) \
+	struct cleanup_##name members;                                        \
+	static inline void cleanup_##name##_destroy_fn(                       \
+		[[maybe_unused]] struct cleanup_##name *ctx) fn_body;         \
+	static inline struct cleanup_##name cleanup_##name##_init_fn(         \
+		init_args) init_body
 
-cleanup_class(mutex, { struct kmutex *mutex; }, { kmutex_release(ctx->mutex); });
-
-static inline struct cleanup_mutex cleanup_create_mutex(struct kmutex *mutex)
-{
-	EXPECT(kmutex_acquire(mutex, TIMEOUT_INFINITE));
-	return (struct cleanup_mutex){ .mutex = mutex };
-}
+#define RET(clazz, initializer) return (struct cleanup_##clazz)initializer;
 
 #define PASTE(a, b) a##b
 #define EXPAND_AND_PASTE(a, b) PASTE(a, b)
 
-#define guard_internal(clazz, tmp, var)        \
-	cleanup_class_##clazz##_t tmp;         \
-	[[gnu::cleanup(cleanup_##clazz##_fn)]] \
-	struct cleanup_##clazz var =           \
-		_Generic(tmp, cleanup_class_mutex_t: cleanup_create_mutex)
+#define GUARD_INTERNAL(clazz, var)                     \
+	[[gnu::cleanup(cleanup_##clazz##_destroy_fn)]] \
+	struct cleanup_##clazz var = cleanup_##clazz##_init_fn
 
-#define guard(clazz)                                                      \
-	guard_internal(clazz, EXPAND_AND_PASTE(__guard_sel, __COUNTER__), \
-		       EXPAND_AND_PASTE(__cleanup_##clazz##_var_,         \
-					__COUNTER__))
+#define guard(clazz) \
+	GUARD_INTERNAL(clazz, EXPAND_AND_PASTE(__cleanup_##clazz, __COUNTER__))
+
+#define FOR_CLASS_LIST(DO)                                               \
+	DO(                                                              \
+		mutex, { struct kmutex *mutex; },                        \
+		{ kmutex_release(ctx->mutex); },                         \
+		{                                                        \
+			EXPECT(kmutex_acquire(mutex, TIMEOUT_INFINITE)); \
+			RET(mutex, { .mutex = mutex });                  \
+		},                                                       \
+		struct kmutex *mutex)
+
+FOR_CLASS_LIST(DEFINE_CLEANUP_CLASS)
