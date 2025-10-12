ae48457bdfbc4764df73252a576c9be3eb8ce250
Vincent Schweiger <vincent.schweiger@xolley.de>
2025-10-12 13:50:18 +0200

    feat: enable stack protector

---
+++ kernel/src/rt/stack.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include <yak/panic.h>
+
+#if UINT32_MAX == UINTPTR_MAX
+#define STACK_CHK_GUARD 0xe2dee396
+#else
+#define STACK_CHK_GUARD 0x595e9fbd94fda766
+#endif
+
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+[[gnu::noreturn, gnu::cold]]
+void __stack_chk_fail(void)
+{
+	panic("stack smashing detected\n");
+	__builtin_trap();
+}
