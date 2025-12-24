01a0ea061696f0ede61a3edcf0c3fd869c3e9171
Vincent <vinsch32@icloud.com>
2025-12-24 15:04:20 +0100

    feat: make target triplet variable

--- toolchains/Generic.cmake
+++ toolchains/Generic.cmake
@@ -13,6 +13,7 @@ endmacro()
 
 require_env(YAK_ARCH)
 require_env(YAK_TOOLCHAIN)
+require_env(YAK_TRIPLET)
 
 set(CMAKE_SYSTEM_NAME "Yak")
 set(CMAKE_SYSTEM_PROCESSOR ${YAK_ARCH})
@@ -41,7 +42,6 @@ add_link_options(
 )
 
 if(YAK_ARCH STREQUAL "x86_64")
-	set(YAK_TARGET x86_64-unknown-yak-kernel)
 	add_compile_options(
 		-mno-red-zone
 		-mno-mmx -mno-sse -mno-sse2
@@ -52,19 +52,18 @@ else()
 endif()
 
 if(YAK_TOOLCHAIN STREQUAL "llvm")
-	set(YAK_TARGET x86_64-unknown-linux-elf)
 	set(CMAKE_C_COMPILER clang)
 	set(CMAKE_CXX_COMPILER clang++)
 	set(CMAKE_ASM_COMPILER clang)
-	add_compile_options(--target=${YAK_TARGET})
+	add_compile_options(--target=${YAK_TRIPLET})
 	add_link_options(
-		--target=${YAK_TARGET}
+		--target=${YAK_TRIPLET}
 		-fuse-ld=lld
 	)
 elseif(YAK_TOOLCHAIN STREQUAL "gcc")
-	set(CMAKE_C_COMPILER ${YAK_TARGET}-gcc)
-	set(CMAKE_CXX_COMPILER ${YAK_TARGET}-g++)
-	set(CMAKE_ASM_COMPILER ${YAK_TARGET}-gcc)
+	set(CMAKE_C_COMPILER ${YAK_TRIPLET}-gcc)
+	set(CMAKE_CXX_COMPILER ${YAK_TRIPLET}-g++)
+	set(CMAKE_ASM_COMPILER ${YAK_TRIPLET}-gcc)
 else()
 	message(FATAL_ERROR "Unsupported YAK_TOOLCHAIN: ${YAK_TOOLCHAIN}")
 endif()
